cf01f93d411910a14643fee1518e8c2330b7eacb
hyunsung.shin <cordorshs@gmail.com>
2019-12-25 23:17:10 +0900

    Project update

--- MVP/SampleProject/app/src/main/java/com/nanamare/mac/sample/data/coin/CoinSource.kt
+++
@@ -1,8 +0,0 @@
-package com.nanamare.mac.sample.data.coin
-
-import com.nanamare.mac.sample.api.upbit.TickerModel
-import com.nanamare.mac.sample.base.BaseSource
-
-interface CoinSource : BaseSource {
-    fun getCoins(ticketList: MutableList<String>, success: (List<TickerModel>) -> Unit, failed: () -> Unit)
-}
--- MVVM/SampleProject/app/src/main/java/com/nanamare/mac/sample/base/BaseSource.kt
+++
@@ -1,5 +0,0 @@
-package com.nanamare.mac.sample.base
-
-interface BaseSource {
-    fun close()
-}
